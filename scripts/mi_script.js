7daf45ba56d97f77d85eaaaa176ef9d2f48ff3f7
Alejandro G. Vera <eldiosvolador@hotmail.com>
2022-12-17 18:38:03 -0300

    terminada la primera versión

--- scripts/mi_script.js
+++ scripts/mi_script.js
@@ -3,6 +3,6 @@ const switcher = document.getElementById('gracias');
 switcher.addEventListener('click', function () {
 
     alert("Gracias por visitar mi sitio, utilicé varios snnipets, cosas que aprendí de las clases de la Unpaz, y de los cursos de "+
-    "Juan Pablo de la Torre Valdez, aún estoy aprendiendo!!!");
+    "Juan Pablo de la Torre Valdez, además de lo del curso online!!!");
 
 }); 
